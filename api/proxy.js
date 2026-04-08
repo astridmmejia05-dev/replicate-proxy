c2c303b781a7ab5cd58fea5e571e6d4336a0baf4
astridmmejia05-dev <astridmmejia05@gmail.com>
2026-04-08 10:04:29 -0600

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  const token = process.env.REPLICATE_API_TOKEN;
+  const { url, body } = req.body;
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  });
+
+  const data = await response.json();
+  res.status(response.status).json(data);
+}
